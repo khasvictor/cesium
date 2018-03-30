9a1d4e23357ce96b046139bcf14626e1ca575d84
quintain <quintain@foxmail.com>
2018-03-30 16:51:49 +0800

    sample

--- Apps/Sandcastle/gallery/gallery-index.js
+++ Apps/Sandcastle/gallery/gallery-index.js
@@ -1,76 +1,80 @@
 // This file is automatically rebuilt by the Cesium build process.
-var hello_world_index = 69;
+var hello_world_index = 74;
 var gallery_demos = [{
   "name": "3D Models",
-  "date": 1513581954816,
+  "date": 1522199914194,
   "img": "3D Models.jpg"
 }, {
   "name": "3D Models Coloring",
-  "date": 1513581954815,
+  "date": 1522199914193,
   "img": "3D Models Coloring.jpg"
 }, {
   "name": "3D Tiles Adjust Height",
-  "date": 1516242684615,
+  "date": 1522199914194,
   "img": "3D Tiles Adjust Height.jpg"
 }, {
   "name": "3D Tiles BIM",
-  "date": 1516242684615,
+  "date": 1522199914195,
   "img": "3D Tiles BIM.jpg"
 }, {
   "name": "3D Tiles Batch Table Hierarchy",
-  "date": 1516242684615,
+  "date": 1522199914196,
   "img": "3D Tiles Batch Table Hierarchy.jpg"
 }, {
   "name": "3D Tiles Clipping Planes",
-  "date": 1516242684615,
+  "date": 1522199914197,
   "img": "3D Tiles Clipping Planes.jpg"
 }, {
   "name": "3D Tiles Feature Picking",
-  "date": 1513581954820,
+  "date": 1522199914197,
   "img": "3D Tiles Feature Picking.jpg"
 }, {
   "name": "3D Tiles Feature Styling",
-  "date": 1513581954821,
+  "date": 1522199914198,
   "img": "3D Tiles Feature Styling.jpg"
 }, {
   "name": "3D Tiles Formats",
-  "date": 1516242684615,
+  "date": 1522199914199,
   "img": "3D Tiles Formats.jpg"
 }, {
   "name": "3D Tiles Inspector",
-  "date": 1516242684615,
+  "date": 1522199914200,
   "img": "3D Tiles Inspector.jpg"
 }, {
   "name": "3D Tiles Interactivity",
-  "date": 1513581954823,
+  "date": 1522199914201,
   "img": "3D Tiles Interactivity.jpg"
 }, {
   "name": "3D Tiles Interior",
-  "date": 1513581954824,
+  "date": 1522199914201,
   "img": "3D Tiles Interior.jpg"
 }, {
   "name": "3D Tiles Photogrammetry",
-  "date": 1516242684615,
+  "date": 1522199914202,
   "img": "3D Tiles Photogrammetry.jpg"
 }, {
   "name": "3D Tiles Photogrammetry Classification",
-  "date": 1516242684615,
+  "date": 1522199914202,
   "img": "3D Tiles Photogrammetry Classification.jpg"
 }, {
   "name": "3D Tiles Point Cloud",
-  "date": 1516242684615,
+  "date": 1522199914206,
   "img": "3D Tiles Point Cloud.jpg"
 }, {
   "name": "3D Tiles Point Cloud Classification",
-  "date": 1515977548293,
+  "date": 1522199914203,
   "img": "3D Tiles Point Cloud Classification.jpg"
 }, {
+  "name": "3D Tiles Point Cloud Shading",
+  "date": 1522199914204,
+  "img": "3D Tiles Point Cloud Shading.jpg"
+}, {
   "name": "3D Tiles Point Cloud Styling",
-  "date": 1516242684615,
+  "date": 1522199914205,
   "img": "3D Tiles Point Cloud Styling.jpg"
 }, {
   "name": "3D Tiles Terrain Classification",
-  "date": 1515977548293,
+  "date": 1522199914206,
   "img": "3D Tiles Terrain Classification.jpg"
 }, {
   "name": "ArcGIS MapServer",
@@ -78,7 +82,7 @@ var gallery_demos = [{
   "img": "ArcGIS MapServer.jpg"
 }, {
   "name": "ArcticDEM",
-  "date": 1515977548293,
+  "date": 1522199914207,
   "img": "ArcticDEM.jpg"
 }, {
   "name": "Atmosphere Color",
@@ -86,15 +90,19 @@ var gallery_demos = [{
   "img": "Atmosphere Color.jpg"
 }, {
   "name": "Billboards",
-  "date": 1513581954830,
+  "date": 1522199914209,
   "img": "Billboards.jpg"
+}, {
+  "name": "Blue Marble",
+  "date": 1522199914210,
+  "img": "Blue Marble.jpg"
 }, {
   "name": "Box",
   "date": 1513581954831,
   "img": "Box.jpg"
 }, {
   "name": "CZML",
-  "date": 1513581954851,
+  "date": 1522199914217,
   "img": "CZML.jpg"
 }, {
   "name": "CZML Billboard and Label",
@@ -122,27 +130,27 @@ var gallery_demos = [{
   "img": "CZML Corridor.jpg"
 }, {
   "name": "CZML Custom Properties",
-  "date": 1515977548293,
+  "date": 1522199914211,
   "img": "CZML Custom Properties.jpg"
 }, {
   "name": "CZML Model",
-  "date": 1513581954839,
+  "date": 1522199914212,
   "img": "CZML Model.jpg"
 }, {
   "name": "CZML Model - Node Transformations",
-  "date": 1513581954838,
+  "date": 1522199914212,
   "img": "CZML Model - Node Transformations.jpg"
 }, {
   "name": "CZML Path",
-  "date": 1513581954840,
+  "date": 1522199914213,
   "img": "CZML Path.jpg"
 }, {
   "name": "CZML Point",
-  "date": 1513581954842,
+  "date": 1522199914215,
   "img": "CZML Point.jpg"
 }, {
   "name": "CZML Point - Time Dynamic",
-  "date": 1513581954841,
+  "date": 1522199914214,
   "img": "CZML Point - Time Dynamic.jpg"
 }, {
   "name": "CZML Polygon",
@@ -150,11 +158,11 @@ var gallery_demos = [{
   "img": "CZML Polygon.jpg"
 }, {
   "name": "CZML Polygon - Interpolating References",
-  "date": 1513581954842,
+  "date": 1522199914215,
   "img": "CZML Polygon - Interpolating References.jpg"
 }, {
   "name": "CZML Polygon - Intervals, Availability",
-  "date": 1513581954844,
+  "date": 1522199914216,
   "img": "CZML Polygon - Intervals, Availability.jpg"
 }, {
   "name": "CZML Polyline",
@@ -182,11 +190,11 @@ var gallery_demos = [{
   "img": "CZML Wall.jpg"
 }, {
   "name": "Callback Property",
-  "date": 1513581954852,
+  "date": 1522199914218,
   "img": "Callback Property.jpg"
 }, {
   "name": "Camera",
-  "date": 1513581954854,
+  "date": 1522199914218,
   "img": "Camera.jpg"
 }, {
   "name": "Camera Tutorial",
@@ -194,31 +202,35 @@ var gallery_demos = [{
   "img": "Camera Tutorial.jpg"
 }, {
   "name": "Cardboard",
-  "date": 1513581954855,
+  "date": 1522199914219,
   "img": "Cardboard.jpg"
 }, {
   "name": "Cesium Inspector",
-  "date": 1513581954855,
+  "date": 1522199914220,
   "img": "Cesium Inspector.jpg"
 }, {
   "name": "Cesium Widget",
   "date": 1513581954856,
   "img": "Cesium Widget.jpg"
 }, {
+  "name": "Cesium World Terrain",
+  "date": 1522199914220,
+  "img": "Cesium World Terrain.jpg"
+}, {
   "name": "Circles and Ellipses",
   "date": 1513581954857,
   "img": "Circles and Ellipses.jpg"
 }, {
   "name": "Classification",
-  "date": 1513581954859,
+  "date": 1522199914222,
   "img": "Classification.jpg"
 }, {
   "name": "Classification Types",
-  "date": 1513581954858,
+  "date": 1522199914221,
   "img": "Classification Types.jpg"
 }, {
   "name": "Clock",
-  "date": 1513581954860,
+  "date": 1522199914222,
   "img": "Clock.jpg"
 }, {
   "name": "Clustering",
@@ -230,20 +242,28 @@ var gallery_demos = [{
   "img": "Corridor.jpg"
 }, {
   "name": "Custom DataSource",
-  "date": 1513581954862,
+  "date": 1522199914223,
   "img": "Custom DataSource.jpg"
 }, {
   "name": "Custom Geocoder",
-  "date": 1513581954863,
+  "date": 1522199914224,
   "img": "Custom Geocoder.jpg"
 }, {
   "name": "Cylinders and Cones",
   "date": 1513581954863,
   "img": "Cylinders and Cones.jpg"
 }, {
+  "name": "DataSource Ordering",
+  "date": 1522199914224,
+  "img": "DataSource Ordering.jpg"
+}, {
   "name": "Distance Display Conditions",
   "date": 1513581954864,
   "img": "Distance Display Conditions.jpg"
+}, {
+  "name": "Earth at Night",
+  "date": 1522199914225,
+  "img": "Earth at Night.jpg"
 }, {
   "name": "GeoJSON and TopoJSON",
   "date": 1513581954865,
@@ -254,11 +274,11 @@ var gallery_demos = [{
   "img": "GeoJSON simplestyle.jpg"
 }, {
   "name": "Geometry and Appearances",
-  "date": 1513581954866,
+  "date": 1522199914226,
   "img": "Geometry and Appearances.jpg"
 }, {
   "name": "Globe Materials",
-  "date": 1513644478010,
+  "date": 1522199914227,
   "img": "Globe Materials.jpg"
 }, {
   "name": "Google Earth Enterprise",
@@ -266,7 +286,7 @@ var gallery_demos = [{
   "img": "Google Earth Enterprise.jpg"
 }, {
   "name": "Ground Clamping",
-  "date": 1513581954869,
+  "date": 1522199914230,
   "img": "Ground Clamping.jpg"
 }, {
   "name": "HTML Overlays",
@@ -274,7 +294,7 @@ var gallery_demos = [{
   "img": "HTML Overlays.jpg"
 }, {
   "name": "HeadingPitchRoll",
-  "date": 1513581954871,
+  "date": 1522199914231,
   "img": "HeadingPitchRoll.jpg"
 }, {
   "name": "Hello World",
@@ -286,7 +306,7 @@ var gallery_demos = [{
   "img": "Imagery Adjustment.jpg"
 }, {
   "name": "Imagery Layers",
-  "date": 1515977548303,
+  "date": 1522199914233,
   "img": "Imagery Layers.jpg"
 }, {
   "name": "Imagery Layers Manipulation",
@@ -294,15 +314,15 @@ var gallery_demos = [{
   "img": "Imagery Layers Manipulation.jpg"
 }, {
   "name": "Imagery Layers Split",
-  "date": 1515977548303,
+  "date": 1522199914231,
   "img": "Imagery Layers Split.jpg"
 }, {
   "name": "Imagery Layers Texture Filters",
-  "date": 1513581954874,
+  "date": 1522199914232,
   "img": "Imagery Layers Texture Filters.jpg"
 }, {
   "name": "Interpolation",
-  "date": 1513581954876,
+  "date": 1522199914233,
   "img": "Interpolation.jpg"
 }, {
   "name": "KML",
@@ -318,7 +338,7 @@ var gallery_demos = [{
   "img": "Labels.jpg"
 }, {
   "name": "LocalToFixedFrame",
-  "date": 1513581954879,
+  "date": 1522199914234,
   "img": "LocalToFixedFrame.jpg"
 }, {
   "name": "Map Pins",
@@ -329,24 +349,32 @@ var gallery_demos = [{
   "date": 1513581954880,
   "img": "Materials.jpg"
 }, {
+  "name": "Multi-part CZML",
+  "date": 1522199914235,
+  "img": "Multi-part CZML.jpg"
+}, {
+  "name": "Natural Earth II",
+  "date": 1522199914236,
+  "img": "Natural Earth II.jpg"
+}, {
   "name": "Offline",
   "date": 1513581954881,
   "img": "Offline.jpg"
 }, {
-  "name": "PATerrain",
-  "date": 1513581954882,
-  "img": "PATerrain.jpg"
+  "name": "PAMAP Terrain",
+  "date": 1522199914652,
+  "img": "PAMAP Terrain.jpg"
 }, {
   "name": "Particle System",
-  "date": 1513581954883,
+  "date": 1522199914239,
   "img": "Particle System.jpg"
 }, {
   "name": "Particle System Fireworks",
-  "date": 1513581954883,
+  "date": 1522199914238,
   "img": "Particle System Fireworks.jpg"
 }, {
   "name": "Physically-Based Materials",
-  "date": 1513581954884,
+  "date": 1522199914240,
   "img": "Physically-Based Materials.jpg"
 }, {
   "name": "Picking",
@@ -370,7 +398,7 @@ var gallery_demos = [{
   "img": "Polyline.jpg"
 }, {
   "name": "Polyline Dash",
-  "date": 1513581954887,
+  "date": 1522199914241,
   "img": "Polyline Dash.jpg"
 }, {
   "name": "Polyline Volume",
@@ -386,15 +414,23 @@ var gallery_demos = [{
   "img": "Rectangle.jpg"
 }, {
   "name": "Reverse Geocoder",
-  "date": 1513581954891,
+  "date": 1522199914242,
   "img": "Reverse Geocoder.jpg"
 }, {
   "name": "Rotatable 2D Map",
   "date": 1513581954891,
   "img": "Rotatable 2D Map.jpg"
 }, {
+  "name": "Scene Rendering Performance",
+  "date": 1522199914242,
+  "img": "Scene Rendering Performance.jpg"
+}, {
+  "name": "Sentinel-2",
+  "date": 1522199914244,
+  "img": "Sentinel-2.jpg"
+}, {
   "name": "Shadows",
-  "date": 1513581954892,
+  "date": 1522199914245,
   "img": "Shadows.jpg"
 }, {
   "name": "Show or Hide Entities",
@@ -410,208 +446,34 @@ var gallery_demos = [{
   "img": "Star Burst.jpg"
 }, {
   "name": "Terrain",
-  "date": 1513581954898,
+  "date": 1522199914247,
   "img": "Terrain.jpg"
 }, {
   "name": "Terrain Clipping Planes",
-  "date": 1514968490753,
+  "date": 1522199914245,
   "img": "Terrain Clipping Planes.jpg"
 }, {
   "name": "Terrain Exaggeration",
-  "date": 1513581954897,
+  "date": 1522199914246,
   "img": "Terrain Exaggeration.jpg"
 }, {
   "name": "Video",
-  "date": 1513581954898,
+  "date": 1522199914248,
   "img": "Video.jpg"
 }, {
   "name": "Wall",
   "date": 1513581954899,
   "img": "Wall.jpg"
 }, {
+  "name": "Washington DC 2017",
+  "date": 1522199914248,
+  "img": "Washington DC 2017.jpg"
+}, {
   "name": "Web Map Service (WMS)",
   "date": 1513581954899,
   "img": "Web Map Service (WMS).jpg"
 }, {
   "name": "Web Map Tile Service with Time",
-  "date": 1513581954900,
+  "date": 1522199914249,
   "img": "Web Map Tile Service with Time.jpg"
-}, {
-  "name": "development/3D Models",
-  "date": 1513581954903,
-  "img": "development/3D Models.jpg"
-}, {
-  "name": "development/3D Models Instancing",
-  "date": 1513581954901,
-  "img": "development/3D Models Instancing.jpg"
-}, {
-  "name": "development/3D Models Node Explorer",
-  "date": 1513581954902,
-  "img": "development/3D Models Node Explorer.jpg"
-}, {
-  "name": "development/BillboardClampToGround",
-  "date": 1513581954903
-}, {
-  "name": "development/Billboards",
-  "date": 1513581954905,
-  "img": "development/Billboards.jpg"
-}, {
-  "name": "development/Billboards Instancing",
-  "date": 1513581954904,
-  "img": "development/Billboards Instancing.jpg"
-}, {
-  "name": "development/Box",
-  "date": 1513581954906,
-  "img": "development/Box.jpg"
-}, {
-  "name": "development/Box Outline",
-  "date": 1513581954905,
-  "img": "development/Box Outline.jpg"
-}, {
-  "name": "development/Circle",
-  "date": 1513581954908,
-  "img": "development/Circle.jpg"
-}, {
-  "name": "development/Circle Outline",
-  "date": 1513581954907,
-  "img": "development/Circle Outline.jpg"
-}, {
-  "name": "development/Corridor",
-  "date": 1513581954909,
-  "img": "development/Corridor.jpg"
-}, {
-  "name": "development/Corridor Outline",
-  "date": 1513581954909,
-  "img": "development/Corridor Outline.jpg"
-}, {
-  "name": "development/Cylinder",
-  "date": 1513581954911,
-  "img": "development/Cylinder.jpg"
-}, {
-  "name": "development/Cylinder Outline",
-  "date": 1513581954910,
-  "img": "development/Cylinder Outline.jpg"
-}, {
-  "name": "development/Display Conditions",
-  "date": 1513581954912
-}, {
-  "name": "development/Ellipse",
-  "date": 1513581954913,
-  "img": "development/Ellipse.jpg"
-}, {
-  "name": "development/Ellipse Outline",
-  "date": 1513581954912,
-  "img": "development/Ellipse Outline.jpg"
-}, {
-  "name": "development/Ellipsoid",
-  "date": 1513581954915,
-  "img": "development/Ellipsoid.jpg"
-}, {
-  "name": "development/Ellipsoid Outline",
-  "date": 1513581954913,
-  "img": "development/Ellipsoid Outline.jpg"
-}, {
-  "name": "development/Ellipsoid Surface",
-  "date": 1513581954914,
-  "img": "development/Ellipsoid Surface.jpg"
-}, {
-  "name": "development/Fog",
-  "date": 1513581954916
-}, {
-  "name": "development/Frustum",
-  "date": 1513581954916
-}, {
-  "name": "development/Geometry and Appearances",
-  "date": 1513581954917,
-  "img": "development/Geometry and Appearances.jpg"
-}, {
-  "name": "development/Ground Primitive",
-  "date": 1513581954918,
-  "img": "development/Ground Primitive.jpg"
-}, {
-  "name": "development/Labels",
-  "date": 1513581954919,
-  "img": "development/Labels.jpg"
-}, {
-  "name": "development/Material",
-  "date": 1513581954920,
-  "img": "development/Material.jpg"
-}, {
-  "name": "development/Multiple Shadows",
-  "date": 1513581954921
-}, {
-  "name": "development/Per Instance Color",
-  "date": 1513581954921,
-  "img": "development/Per Instance Color.jpg"
-}, {
-  "name": "development/PointPrimitives",
-  "date": 1513581954922,
-  "img": "development/PointPrimitives.jpg"
-}, {
-  "name": "development/Polygon",
-  "date": 1513581954924,
-  "img": "development/Polygon.jpg"
-}, {
-  "name": "development/Polygon Outline",
-  "date": 1513581954923,
-  "img": "development/Polygon Outline.jpg"
-}, {
-  "name": "development/Polyline",
-  "date": 1513581954928,
-  "img": "development/Polyline.jpg"
-}, {
-  "name": "development/Polyline Color",
-  "date": 1513581954925,
-  "img": "development/Polyline Color.jpg"
-}, {
-  "name": "development/Polyline Material",
-  "date": 1513581954925,
-  "img": "development/Polyline Material.jpg"
-}, {
-  "name": "development/Polyline Volume",
-  "date": 1513581954927,
-  "img": "development/Polyline Volume.jpg"
-}, {
-  "name": "development/Polyline Volume Outline",
-  "date": 1513581954926,
-  "img": "development/Polyline Volume Outline.jpg"
-}, {
-  "name": "development/Polylines",
-  "date": 1513581954929,
-  "img": "development/Polylines.jpg"
-}, {
-  "name": "development/Rectangle",
-  "date": 1513581954931,
-  "img": "development/Rectangle.jpg"
-}, {
-  "name": "development/Rectangle Outline",
-  "date": 1513581954930,
-  "img": "development/Rectangle Outline.jpg"
-}, {
-  "name": "development/Shadows",
-  "date": 1513581954932
-}, {
-  "name": "development/Simple Polyline",
-  "date": 1513581954932,
-  "img": "development/Simple Polyline.jpg"
-}, {
-  "name": "development/Sphere",
-  "date": 1513581954934,
-  "img": "development/Sphere.jpg"
-}, {
-  "name": "development/Sphere Outline",
-  "date": 1513581954933,
-  "img": "development/Sphere Outline.jpg"
-}, {
-  "name": "development/Volumes",
-  "date": 1513581954934,
-  "img": "development/Volumes.jpg"
-}, {
-  "name": "development/Wall",
-  "date": 1513581954936,
-  "img": "development/Wall.jpg"
-}, {
-  "name": "development/Wall Outline",
-  "date": 1513581954935,
-  "img": "development/Wall Outline.jpg"
 }];
--- Source/Shaders/Builtin/Constants/passTranslucent.js
+++ Source/Shaders/Builtin/Constants/passTranslucent.js
@@ -1,14 +1,6 @@
 //This file is automatically rebuilt by the Cesium build process.
 define(function() {
     'use strict';
-    return "/**\n\
- * The automatic GLSL constant for {@link Pass#TRANSLUCENT}\n\
- *\n\
- * @name czm_passTranslucent\n\
- * @glslConstant\n\
- *\n\
- * @see czm_pass\n\
- */\n\
-const float czm_passTranslucent = 9.0;\n\
+    return "const float czm_passTranslucent = 9.0;\n\
 ";
 });
